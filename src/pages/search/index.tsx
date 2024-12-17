4afa9af534d893fb90c7ab51a2e4e289d6366933
lyju777 <lyju777@hanmail.net>
2024-12-17 16:17:06 +0900

    next.js install / 프리페칭 / API Routes

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+
+  return <div>search{q}</div>;
+}
